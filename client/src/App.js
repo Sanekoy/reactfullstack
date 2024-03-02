4065a605be7a01e1d069275787ca5bd0079607eb
Sanekoy <sanek92003@gmail.com>
2024-03-02 16:15:03 +0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <button > 
+      <button color="green" shadowSize={2}> 
         hello world
       </button>
       <label>
